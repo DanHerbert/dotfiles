2dd0d7acfec9ffc6c030abf294864d0a8615c807
Dan Herbert <dotfiles-dan@hrbrt.co>
2023-08-25 18:09:15 -0700

    Convert some scripts to functions

--- zshrc.d/35_functions.zsh
+++ zshrc.d/35_functions.zsh
@@ -1,1 +1,27 @@
 #!/usr/bin/zsh
+
+lanip() {
+    /usr/bin/ip -4 -o addr show scope global | awk '{gsub(/\/.*/,"",$4); print $4}'
+}
+export lanip
+
+lanips() {
+    /usr/bin/ip -o addr show scope global | awk '{gsub(/\/.*/,"",$4); print $4}'
+}
+export lanips
+
+wanip() {
+    curl --write-out '\n' --silent 'https://ipv4.dan.herbert.io/remote_addr'
+}
+export wanip
+
+wanip6() {
+    curl --write-out '\n' --silent 'https://ipv6.dan.herbert.io/remote_addr'
+}
+export wanip6
+
+wanips() {
+    wanip
+    wanip6
+}
+export wanip6
