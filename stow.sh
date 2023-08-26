092ef34504405490c6461eecf5a8b63161ca7a4f
Dan Herbert <dotfiles-dan@hrbrt.co>
2023-08-26 16:43:08 -0700

    Make stow.sh placeholder dir creation simpler
    Use find commands so the script is more robust and maintainable than the
    manual mess from before.

--- stow.sh
+++ stow.sh
@@ -3,29 +3,25 @@
 # when run repeatedly.
 set -eu
 
-# Ensure a few folders really exist so that stow doesn't mess things up by
-# trying to make them symlinks.
-if ! [ -d "$HOME/.config" ]; then
-    mkdir -p "$HOME/.config"
-fi
-if ! [ -d "$HOME/.local/bin" ]; then
-    mkdir -p "$HOME/.local/bin"
-fi
-
-if ! [ -d "$HOME/.local/lib" ]; then
-    mkdir -p "$HOME/.local/lib"
-fi
-
-if ! [ -d "$HOME/.local/lib/bc.d" ]; then
-    mkdir -p "$HOME/.local/lib/bc.d"
-fi
-
-if ! command -v stow 2> /dev/null; then
+if ! command -v stow > /dev/null 2>&1; then
     echo 'GNU stow is not installed. All package managers call this "stow".'
     exit 1
 fi
 
-set -v
+# Ensure a few folders really exist so that stow doesn't mess things up by
+# trying to make them symlinks.
+if [ ! -d "$HOME/.config" ]; then
+    mkdir -p "$HOME/.config"
+fi
+
+# vim plugins are the 1 thing that should always be symlinked if possible and do
+# not need a placeholder directory.
+(cd home && find . -maxdepth 4 -type d -exec mkdir -p "$HOME/{}" \;)
+(cd local && find . -maxdepth 4 -type d -exec mkdir -p "$HOME/.local/{}" \;)
+(cd config && find . -maxdepth 4 -type d -exec mkdir -p "$HOME/.config/{}" \;)
+echo 'Placeholder directories for stow targets have been created, if needed.'
+
+set -x
 stow --target="$HOME/.config" config
 stow --target="$HOME/.local" local
 stow --target="$HOME" home
