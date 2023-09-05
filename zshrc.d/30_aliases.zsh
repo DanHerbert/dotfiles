f5476611ff0e1cf138d432c2954c37359cdf6688
Dan Herbert <dotfiles-dan@hrbrt.co>
2023-09-05 14:22:50 -0700

    More ls aliases

--- zshrc.d/30_aliases.zsh
+++ zshrc.d/30_aliases.zsh
@@ -14,7 +14,8 @@ alias ......='cd ../../../../..'
 
 # Environment variable LS_COLORS must also exist for this to work.
 alias ls='ls --color=auto --group-directories-first -p'
-alias lsc='ls --color=auto --group-directories-first -pA'
+alias lsa='ls --color=auto --group-directories-first -pA'
+alias lst='ls --color=auto --group-directories-first -pt'
 
 # Force diff to use color by default.
 alias diff='diff --color'
