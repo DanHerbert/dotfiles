d24874469264b3a7c09774e4e7df2005426ca416
Dan Herbert <dotfiles-dan@hrbrt.co>
2023-09-07 10:42:29 -0700

    Fix cargo & golang PATH locations
    This was apparently broken in commit dd1d0687 since I forgot to edit my
    PATH after setting different locations for these apps

--- zshrc.d/20_path.zsh
+++ zshrc.d/20_path.zsh
@@ -4,14 +4,5 @@
 typeset -U path PATH
 
 path=("$HOME/.local/bin" "$path[@]")
-path=("$HOME/.golang/bin" "$path[@]")
-path=("$HOME/.cargo/bin" "$path[@]")
-path=("$HOME/.npm-packages/bin" "$path[@]")
-
-if [[ -d "$HOME/.npm-packages/bin" ]]; then
-    export NODE_PATH="$HOME/.npm-packages/bin"
-fi
-
-if command -v go > /dev/null 2>&1 && [[ -d "$HOME/golang/bin" ]]; then
-    export GOPATH="$HOME/.golang"
-fi
+path=("$GOPATH/bin" "$path[@]")
+path=("$CARGO_HOME/bin" "$path[@]")
