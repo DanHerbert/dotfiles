d11c2c8f20d3766b58ee8316482d73d62ca1c400
Dan Herbert <dotfiles-dan@hrbrt.co>
2024-01-28 15:23:26 -0500

    Add rustup to PATH

--- lib/zshrc.d/20_path.zsh
+++ lib/zshrc.d/20_path.zsh
@@ -3,6 +3,7 @@
 # Make PATH only have unique values. Later entries are removed by earlier ones.
 typeset -U path PATH
 
-path=("$HOME/.local/bin" "$path[@]")
-path=("$GOPATH/bin" "$path[@]")
+path=("$RUSTUP_HOME/bin" "$path[@]")
 path=("$CARGO_HOME/bin" "$path[@]")
+path=("$GOPATH/bin" "$path[@]")
+path=("$HOME/.local/bin" "$path[@]")
