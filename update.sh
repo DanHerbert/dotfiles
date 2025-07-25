c4633250ef1eb48b2ccc28d434537c6751dedd5c
Dan Herbert <dotfiles-dan@hrbrt.co>
2025-07-25 10:59:25 -0400

    source stow.sh within update script

--- update.sh
+++ update.sh
@@ -46,7 +46,7 @@
         git stash pop
     fi
     if [ "$new_version" != "$old_version" ] || [ "$old_submodule_version" != "$new_submodule_version" ]; then
-        "$PROJECT_ROOT/stow.sh"
+        source "$PROJECT_ROOT/stow.sh"
     fi
 }
 exit
